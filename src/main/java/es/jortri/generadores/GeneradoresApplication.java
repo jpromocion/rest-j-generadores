f8d7a6cfe7840a853859c80d91211ef4934aedd4
Jose Javier Gálvez Gómez <35563646+jpromocion@users.noreply.github.com>
2024-09-04 14:08:54 +0200

    Apit rest base con Demo hola mundo

---
+++ src/main/java/es/jortri/generadores/GeneradoresApplication.java
@@ -0,0 +1,13 @@
+package es.jortri.generadores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneradoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneradoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/jortri/generadores/GeneradoresApplicationTests.java
@@ -0,0 +1,13 @@
+package es.jortri.generadores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneradoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
