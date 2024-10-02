5f0c5b7e1cfd95488a7642d21b28334f36214718
Jose Javier Gálvez Gómez <jpromocion@gmail.com>
2024-10-02 17:29:59 +0200

    Version mostrable
    Se incluye para coger version del pom.xml y sacarlo a una propiedad en
    applicatrion properties
    
    SE crea operacion demo/version para obtener la version.

--- src/main/java/es/jortri/generadores/controller/DemoController.java
+++ src/main/java/es/jortri/generadores/controller/DemoController.java
@@ -1,5 +1,6 @@
 package es.jortri.generadores.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
+	
+	@Value("${build.version}")
+	private String buildVersion;
 
 	@GetMapping("/hola")
 	public String hola(){
 		return "Hola Mundo";
 	}
+	
+	@GetMapping("/version")
+	public String version(){
+		return "v." + buildVersion;
+	}	
 	
 }
